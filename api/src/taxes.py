138f4166aeb8605058af18c6f79828e01bb29288
leonardKleber <leonard.kleber@outlook.com>
2025-12-01 21:12:09 +0100

    Provide template for taxes.py

---
+++ api/src/taxes.py
@@ -0,0 +1,20 @@
+"""
+This file handles all functionality required for the taxes page.
+"""
+
+
+def generate_tax_queue():
+    """
+    This function iterates through all transactions for a single asset and puts shares in a queue
+    to maintain the FIFO accounting principle. It then returns the queue.
+    """
+    return
+
+
+def generate_tax_free_amount():
+    """
+    This function returns the number of shares for an asset that can be sold without any tax
+    implications. Under German regulations, this means that any share held for more than one year
+    is tax-free.
+    """
+    return
